37bbbca67917e9946855f5bc513ebf086297d0be
Nyand <rbrtmt100@gmail.com>
2024-07-05 03:54:12 -0700

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user with the
+letter as a parameter."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        payload = {'q': sys.argv[1]}
+        r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+        res = r.json()
+        if len(res) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(res['id'], res['name']))
+    except ValueError:
+        print("Not a valid JSON")
+    except IndexError:
+        print("No result")
